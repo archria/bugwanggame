a76756acb1a9bca5614e5894b8538c6a91a5f73d
archria <jonghyun95@kakao.com>
2021-08-04 21:25:46 +0900

    3강까지 따라하기 완료

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,27 +7,49 @@ public class PlayerMove : MonoBehaviour
     public float maxSpeed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    private void Start() {
+        Debug.Log("start");        
+    }
 
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        //Stop speed
+        if(Input.GetButtonUp("Horizontal")) // up = 떼는거
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.2f , rigid.velocity.y);
+        //directon sprite
+        if(Input.GetButton("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1; // 왼쪽일때 flip x
+        
+        //Animation
+        if(Mathf.Abs( rigid.velocity.x ) < 0.3)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         float h = Input.GetAxisRaw("Horizontal");
-        
+
         // move by key control
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse); // accelerator
+        //rigid.velocity = new Vector2(maxSpeed*h, rigid.velocity.y); // fixed velocity
         if(rigid.velocity.x > maxSpeed) // right max speed
             rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
         else if (rigid.velocity.x < maxSpeed * (-1)) // left max speed
-            rigid.velocity = new Vector2(maxSpeed,rigid.velocity.y);
+            rigid.velocity = new Vector2(maxSpeed * (-1) ,rigid.velocity.y);
 
-        //change username
-        // github desktop working?
-        //how about now?
     }
 }
