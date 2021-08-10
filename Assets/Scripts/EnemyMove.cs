1b57ab1eb4785b6a92abaa85ee2ce6af5bbae1c4
archria <jonghyun95@kakao.com>
2021-08-10 23:34:33 +0900

    learned about invoke, recursive

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class EnemyMove : MonoBehaviour
 {
     Rigidbody2D rigid;
-    
+    public int nextMove;
     private void Awake() {
         rigid = GetComponent<Rigidbody2D>();
+        Think();
+
+        Invoke("Think",5);
     }
     // Start is called before the first frame update
     void Start()
@@ -16,13 +19,17 @@ public class EnemyMove : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        rigid.velocity = new Vector2(-1,rigid.velocity.y);
-        
+        rigid.velocity = new Vector2(nextMove,rigid.velocity.y);
+    }
+
+    void Think(){
+        nextMove = Random.Range(-1,2);
+
+        Invoke("Think",5);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
